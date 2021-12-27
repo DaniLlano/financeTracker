e416167f410f2fb8cd255a56535fa1befc99985c
Dani <danillano97@gmail.com>
2021-12-26 23:38:37 -0300

    Form

--- src/Components/Main/Form/Form.jsx
+++ src/Components/Main/Form/Form.jsx
@@ -18,7 +18,87 @@ const initialState = {
 const NewTransactionForm = () => {
     const classes = useStyles();
     const { addTransaction } = useContext(ExpenseTrackerContext);
-    const [ formDate, setFormDate ] = useState(initialState);
+    const [ formData, setFormData ] = useState(initialState);
     const { segment } = useSpeechContext();
     const [ open, setOpen] = React.useState(false);
+
+    const createTransaction = () => {
+        if (Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
+
+        if (incomeCategories.map((iC) => iC.type).includes(formData.category)) {
+            setFormData({...formData, type: 'Income'});
+        } else if (expenseCategories.map((iC) => iC.type).includes(formData.category)) {
+            setFormData({...formData, type: 'Expense'});
+        }
+
+        setOpen(true);
+        addTransaction({...formData, amount: Number(formData.amount), id: uuidv4() });
+        setFormData(initialState);
+    };
+
+    useEffect(() => {
+        if (segment) {
+            if (segment.intent.intent === 'add_expense') {
+                setFormData({...formData, type: 'Expense'});
+            } else if (segment.intent.intent === 'add_income') {
+                setFormData({...formData, type: 'Income'});
+            } else if (segment.isFinal && segment.intent.intent === 'create_transaction') {
+                return createTransaction();
+            } else if (segment.isFinal && segment.intent.intent === 'cancel_transaction') {
+                return setFormData(initialState);
+            }
+
+            segment.entities.forEach((s) => {
+                const category = `${s.value.charAt(0)}${s.value.slice(1).toLocaleLowerCase()}`;
+
+                switch (s.type) {
+                    case 'amount':
+                        setFormData({...formData, amount:s.value});
+                        break;
+                    case 'category':
+                        if (incomeCategories.map((iC) => iC.type).includes(category)) {
+                            setFormData({...formData, type: 'Income', category});
+                        } else if (expenseCategories.map((iC) => iC.type).includes(category)) {
+                            setFormData({...formData, type: 'Expense', category});
+                        }
+                        break;
+                    case 'date':
+                        setFormData({...formData, date: s.value});
+                        break;
+                    default:
+                        break;
+                };
+            });
+
+            if (segment.isFinal && formData.amount && formData.category && formData.type && formData.date) {
+                createTransaction();
+            }
+        }
+    }, [segment]);
+
+    const selectedCategories = formData.type === 'Income' ? incomeCategories : expenseCategories;
+
+    return (
+        <Grid>
+            <Snackbar open={open} setOpen={setOpen} />
+            <Grid item xs={12}>
+                <Typography align="center" variant="subtitle2" gutterBottom>
+                    {segment ? (
+                        <div>
+                            {segment.words.map((w) => w.value).join(" ")}
+                        </div>
+                    ) : null}
+                </Typography>
+            </Grid>
+            <Grid item xs={6}>
+                <FormControl fullWidth>
+                    <InputLabel1>Type</InputLabel1>
+                    <Select value={formatDate.type} onChange={(e) => setFormData({...formData, type: e.target.value})}>
+                        <MenuItem value="Income">Income</MenuItem>
+                        <MenuItem value="Expense">Expense</MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+        </Grid>
+    )
 }
